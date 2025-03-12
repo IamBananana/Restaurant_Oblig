ce69960eacbf73d5366068c148c82fbf014166ea
IamBananana <richardgjelland@hotmail.com>
2025-03-12 12:40:47 +0100

    grids and scenehandler handler handler

--- src/main/java/com/example/oblig_restaurant/HelloApplication.java
+++ src/main/java/com/example/oblig_restaurant/HelloApplication.java
@@ -3,6 +3,9 @@ package com.example.oblig_restaurant;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +13,23 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        final double WIDTH = Screen.getPrimary().getBounds().getWidth()*0.70;
+        final double HEIGHT = Screen.getPrimary().getBounds().getHeight()*0.70;
+        Button button = new Button("Start Simulation");
+
+        button.setOnAction(e -> {
+            try {
+                new SceneHandler().start(new Stage());
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+            stage.close();
+        });
+
+        BorderPane root = new BorderPane();
+        root.setTop(button);
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
+        stage.setTitle("Restaurant Simulation");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/example/oblig_restaurant/SceneHandler.java
@@ -0,0 +1,61 @@
+package com.example.oblig_restaurant;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+public class SceneHandler extends Application {
+    final double WIDTH = Screen.getPrimary().getBounds().getWidth()*0.70;
+    final double HEIGHT = Screen.getPrimary().getBounds().getHeight()*0.70;
+    BorderPane root;
+    VBox leftVBucks;
+    VBox middleVBucks;
+    VBox rightVBucks;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        initializeGrid();
+
+        System.out.println("This ran");
+        Scene scene = new Scene(root, WIDTH, HEIGHT);
+        stage.setTitle("Restaurant Simulation Started");
+        stage.setScene(scene);
+        stage.show();
+    }
+    private void initializeGrid() {
+        root = new BorderPane();
+        GridPane grid = new GridPane();
+        leftVBucks = new VBox();
+        middleVBucks = new VBox();
+        rightVBucks = new VBox();
+        leftVBucks.setStyle("-fx-border-color: black; -fx-border-width: 1px;");
+        middleVBucks.setStyle("-fx-border-color: black; -fx-border-width: 1px;");
+        rightVBucks.setStyle("-fx-border-color: black; -fx-border-width: 1px;");
+        leftVBucks.setPrefSize(WIDTH / 3, HEIGHT);
+        middleVBucks.setPrefSize(WIDTH / 3, HEIGHT);
+        rightVBucks.setPrefSize(WIDTH / 3, HEIGHT);
+
+        VBox.setVgrow(leftVBucks, Priority.ALWAYS);
+        VBox.setVgrow(middleVBucks, Priority.ALWAYS);
+        VBox.setVgrow(rightVBucks, Priority.ALWAYS);
+
+        ColumnConstraints col1 = new ColumnConstraints();
+        col1.setPercentWidth(33.3);
+        ColumnConstraints col2 = new ColumnConstraints();
+        col2.setPercentWidth(33.3);
+        ColumnConstraints col3 = new ColumnConstraints();
+        col3.setPercentWidth(33.3);
+
+        grid.getColumnConstraints().addAll(col1, col2, col3);
+
+        grid.add(leftVBucks, 0, 0);
+        grid.add(middleVBucks, 1, 0);
+        grid.add(rightVBucks, 2, 0);
+
+        grid.setPrefSize(WIDTH, HEIGHT);
+
+        root.setCenter(grid);
+    }
+}
