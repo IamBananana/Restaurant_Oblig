39a99ce781665ac17b6e179d0c3f2d5864f616a2
Erik Runde <erikrunde2@gmail.com>
2025-04-02 12:52:33 +0200

    Changes by ulrikkeekedal

--- src/main/java/com/example/oblig_restaurant/Customer.java
+++ src/main/java/com/example/oblig_restaurant/Customer.java
@@ -11,9 +11,9 @@ public class Customer implements Runnable {
     private Status status;
 
     // Terskler i prosent
-    private final int HAPPY_THRESH = 50;
-    private final int NORMAL_THRESH = 80;
-    private final int ANGRY_THRESH = 100;
+    private final int HAPPY_THRESH = 0;
+    private final int NORMAL_THRESH = 33;
+    private final int ANGRY_THRESH = 67;
 
     // Maks ventetid i sekunder
     private final int MAX_WAIT_SECONDS = 20;
